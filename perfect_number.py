724732e7b0a68ea2ba87d95e3a517a91a44abbfb
Deepak choudhary <115655437+deepakchy1998@users.noreply.github.com>
2022-12-10 15:29:10 +0530

    Add files via upload

---
+++ perfect_number.py
@@ -0,0 +1,9 @@
+num1=int(input("enter your number : "))
+sum=0
+for i in range(1,num1):
+    if num1%i==0:
+        sum+=i
+if sum==num1:
+    print("given number is a perfect number")
+else:
+    print("given number is not a perfect number")
